be1cab54fbbe4d43e04e29b1a920c6483af8a487
Praseetha-KR <praseetha04@gmail.com>
2017-03-03 17:31:52 +0530

    [feature] Adds daily expense view

---
+++ app/expense-view/expense-view.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    ScrollView,
+    ToolbarAndroid,
+} from 'react-native';
+import config from '../../config.js';
+
+export default class ExpenseView extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            expense: { date: '' },
+        }
+
+        this.categoryIconList = {
+            'food': require('../img/food.png'),
+            'travel': require('../img/travel.png'),
+            'supermarket': require('../img/supermarket.png'),
+            'recharge': require('../img/recharge.png'),
+            'e-shopping': require('../img/e_shopping.png'),
+            'rent/wifi/hosting': require('../img/rent_wifi_hosting.png'),
+            'transfers': require('../img/transfers.png'),
+            'others': require('../img/others.png'),
+        };
+    }
+    componentWillMount() {
+        this._getExpense()
+            .then(response => {
+                this.setState({
+                    expense: response
+                });
+            });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ToolbarAndroid
+                    style={styles.toolbar}
+                    title={config.APP_NAME}
+                    subtitle='Daily Expense'
+                    actions={toolbarActions}
+                    onActionSelected={(e) => this._onActionSelected(e)}
+                />
+                <ScrollView style={styles.scroll}>
+                    <View style={styles.expense}>
+                        <Text style={styles.date}>{this.state.expense['date']}</Text>
+                        <View style={[styles.total, this._getExpenseSlabColors(this.state.expense['total'])]}>
+                            <Text style={styles.total__count}>&#8377;&nbsp;{this.state.expense['total']}</Text>
+                        </View>
+                        {this._renderCategories(this.state.expense)}
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+
+    _renderCategories(expense) {
+        const categoryIconList = config.CATEGORY_ICONS;
+        return (
+            <View style={styles.categories}>
+                {Object.keys(categoryIconList).map((key) => {
+                    return (
+                        <View key={key} style={styles.category}>
+                            <View style={styles.category__name}>
+                                <View style={styles.category__icon_wrapper}>
+                                    <Image
+                                        style={styles.category__icon}
+                                        source={this.categoryIconList[key]}
+                                    />
+                                </View>
+                                <Text style={styles.category__label}>{`${key.charAt(0).toUpperCase()}${key.slice(1)}`}</Text>
+                            </View>
+                            <Text style={styles.category__value}>{expense[key]}</Text>
+                        </View>
+                    )
+                })}
+            </View>
+        );
+    }
+
+    _getExpense() {
+        return fetch(config.EXPENSE_API_URL + '/expenses/' + this.props.date +'/', {
+                method: 'GET'
+            }).then(res => res.json());
+    }
+
+    _onActionSelected(position) {
+        this.props.navigator.pop();
+    }
+
+     _getExpenseSlabColors(number) {
+        const n = parseInt(number);
+        if (n < config.DAILY_TOTAL_LIMIT_SAFE) {
+            return { backgroundColor: config.THEME.safe };
+        }
+        if (n < config.DAILY_TOTAL_LIMIT_WARNING) {
+            return { backgroundColor: config.THEME.warning };
+        }
+        if (n < config.DAILY_TOTAL_LIMIT_DANGER) {
+            return { backgroundColor: config.THEME.danger };
+        }
+        if (n < config.DAILY_TOTAL_LIMIT_EXTREME) {
+            return { backgroundColor: config.THEME.extreme };
+        }
+        if (n > config.DAILY_TOTAL_LIMIT_EXTREME) {
+            return { backgroundColor: config.THEME.overflow };
+        }
+        return { backgroundColor: config.THEME.accent };
+    }
+}
+
+var toolbarActions = [
+    {title: 'x', show: 'always', nav: 'ExpenseList'},
+];
+
+var styles = StyleSheet.create({
+    container: {
+        backgroundColor: config.THEME.primary,
+        flex: 1,
+    },
+    toolbar: {
+        backgroundColor: config.THEME.primary,
+        height: 56,
+    },
+    expense: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    date: {
+        fontSize: 20,
+        marginTop: 20,
+    },
+    total: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 200,
+        height: 200,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        borderRadius: 100,
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    total__count: {
+        fontSize: 50,
+        color: config.THEME.light
+    },
+    categories: {
+        width: 250,
+        marginBottom: 100
+    },
+    category: {
+        borderBottomWidth: 1,
+        borderStyle: 'solid',
+        borderColor: config.THEME.primary_border,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+    },
+    category__name: {
+        flexDirection: 'row',
+    },
+    category__icon_wrapper: {
+        marginRight: 10,
+        width: 25,
+    },
+    category__icon: {
+        width: 22,
+        height: 22,
+    }
+});
