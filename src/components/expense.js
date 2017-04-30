d03848c6541498b0285dc42375dba9e4f059b69a
Praseetha-KR <praseetha04@gmail.com>
2017-05-01 04:24:08 +0530

    Modify content & style for expense item

--- src/components/expense.js
+++ src/components/expense.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     Text,
     View,
+    Image,
     StyleSheet
 } from 'react-native';
 import Button from 'react-native-button';
@@ -13,20 +14,63 @@ export default class Expense extends Component {
         this.state = {}
     }
 
+    _formatDate(date) {
+        const today = new Date();
+        const dateDiff = date.getDate() - today.getDate();
+        const monthDiff = date.getMonth() - today.getMonth();
+        const yearDiff = date.getFullYear() - today.getFullYear();
+        if (!dateDiff && !monthDiff && !yearDiff) {
+            return `Today`;
+        }
+        return date.toDateString().substr(4, 6);
+    }
+
+    _categoryColor(category) {
+        const colorMap = {
+            food: '#FFBF00',
+            shopping: '#FC404E',
+            travel: 'royalblue',
+            utilities: '#77D259',
+            recharge: '#11C6E5',
+            others: '#A66C59'
+        };
+        return {
+            backgroundColor: colorMap[category]
+        };
+    }
+
     render() {
+        const categories = {
+            food: require('../icons/food.png'),
+            shopping: require('../icons/shopping.png'),
+            travel: require('../icons/travel.png'),
+            utilities: require('../icons/utilities.png'),
+            recharge: require('../icons/recharge.png'),
+            others: require('../icons/others.png')
+        };
         return (
             <View style={styles.expense}>
-                <Text style={styles.expense__purpose}>{this.props.purpose}</Text>
-                <Text>{this.props.category}</Text>
-                <Text>{this.props.amount}</Text>
-                <Text>{(new Date(this.props.date)).toString()}</Text>
-                <Button
+                <View style={styles.expense__left}>
+                    {/*<Text style={styles.expense__category}>{this.props.category}</Text>*/}
+                    <View style={[styles.expense__category, this._categoryColor(this.props.category)]}>
+                        <Image source={categories[this.props.category]} style={{width: 25, height: 25}}/>
+                    </View>
+                </View>
+                <View style={styles.expense__mid}>
+                    <Text style={styles.expense__purpose}>{this.props.purpose}</Text>
+                </View>
+                <View style={styles.expense__right}>
+                    <Text style={styles.expense__amount}>{this.props.amount}</Text>
+                    <Text style={styles.expense__date}>{this._formatDate(new Date(this.props.date))}</Text>
+                </View>
+
+                {/*<Button
                     style={styles.button}
                     disabled={!this.props.removable}
                     styleDisabled={styles.buttonDisabled}
                     onPress={() => this.props.onRemove()}>
                     x
-                </Button>
+                </Button>*/}
             </View>
         )
     }
@@ -35,9 +79,17 @@ export default class Expense extends Component {
 var styles = StyleSheet.create({
     expense: {
         backgroundColor: '#FFFFFF',
-        borderColor: '#cccccc',
+        borderColor: '#ebeff3',
         borderWidth: 1,
-        borderStyle: 'solid'
+        borderStyle: 'solid',
+        borderRadius: 3,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginBottom: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     button: {
         borderColor: 'red',
@@ -47,9 +99,43 @@ var styles = StyleSheet.create({
         width: 28,
         height: 28,
         borderRadius: 20,
-        opacity: 0.3,
+        opacity: 0.3
+    },
+    expense__category: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#6D74FC',
+        borderRadius: 20,
+        height: 40,
+        width: 40,
+        alignSelf: 'center'
     },
     expense__purpose: {
-        fontSize: 16
+        fontSize: 14,
+        marginLeft: 6,
+    },
+    expense__date: {
+        color: '#d1d7dc'
+    },
+    expense__amount: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    expense__left: {
+        width: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    expense__mid: {
+        overflow: 'hidden',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        flex: 3
+    },
+    expense__right: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        flexDirection: 'column'
     }
 });
